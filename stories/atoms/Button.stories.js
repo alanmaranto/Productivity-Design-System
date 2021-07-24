08a2085d1ed26f966236e6c7bf4861df67a314dc
alan <alanarostegui@hotmail.com>
2021-07-24 02:17:54 -0500

    atomic design and button component

---
+++ stories/atoms/Button.stories.js
@@ -0,0 +1,14 @@
+import Button from "../../atomic/atoms/Button";
+
+export default {
+  title: "Atoms/Button",
+  component: Button,
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  children: "Primary",
+};
