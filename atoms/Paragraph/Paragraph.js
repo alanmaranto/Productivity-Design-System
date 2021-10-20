a1dbb0efa90ade7be4da83a4fc5ced45bee6661c
alan <alanarostegui@hotmail.com>
2021-10-20 18:27:21 -0500

    feat: add paragraph atom

--- atoms/Paragraph/Paragraph.js
+++ atoms/Paragraph/Paragraph.js
@@ -3,14 +3,31 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 import styles from "./Paragraph.module.css";
 
-const Paragraph = ({ children }) => {
-  return <div className={classNames(styles.component)}>{children}</div>;
+const Paragraph = ({ children, size, color, weight }) => {
+  return (
+    <div
+      className={classNames(styles["paragraph"], {
+        [styles[`color-${color}`]]: color,
+        [styles[`size-${size}`]]: size,
+        [styles[`weight-${weight}`]]: weight,
+      })}
+    >
+      {children}
+    </div>
+  );
 };
 
 Paragraph.propTypes = {
   children: PropTypes.node.isRequired,
+  color: PropTypes.oneOf(["muted", "default"]),
+  size: PropTypes.oneOf(["sm", "md", "lg"]),
+  weight: PropTypes.oneOf(["normal", "medium"]),
 };
 
-Paragraph.defaultProps = {};
+Paragraph.defaultProps = {
+  size: "md",
+  color: "default",
+  weight: "medium",
+};
 
 export default Paragraph;
---
+++ stories/atoms/Paragraph.stories.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Paragraph from "../../atoms/Paragraph";
+
+export default {
+  title: "Atoms/Paragraph",
+  component: Paragraph,
+  decorators: [
+    (Story) => (
+      <div style={{ display: "flex", flexDirection: "column" }}>{Story()}</div>
+    ),
+  ],
+};
+
+const Template = (args) => <Paragraph {...args} />;
+
+export const Default = Template.bind({});
+export const Muted = Template.bind({});
+export const Small = Template.bind({});
+export const Medium = Template.bind({});
+export const Large = Template.bind({});
+export const WeightNormal = Template.bind({});
+export const WeightMedium = Template.bind({});
+
+Default.args = {
+  children: `El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña toca el
+      saxofón detrás del palenque de paja.`,
+  color: "default",
+};
+
+Muted.args = {
+  children: `El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña toca el
+      saxofón detrás del palenque de paja.`,
+  color: "muted",
+};
+
+Small.args = {
+  children: `El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña toca el
+      saxofón detrás del palenque de paja.`,
+  size: "sm",
+};
+
+Medium.args = {
+  children: `El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña toca el
+      saxofón detrás del palenque de paja.`,
+  size: "md",
+};
+
+Large.args = {
+  children: `El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña toca el
+      saxofón detrás del palenque de paja.`,
+  size: "lg",
+};
+
+WeightMedium.args = {
+  children: `El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña toca el
+      saxofón detrás del palenque de paja.`,
+  size: "md",
+  weight: "medium",
+};
+
+WeightNormal.args = {
+  children: `El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña toca el
+      saxofón detrás del palenque de paja.`,
+  size: "md",
+  weight: "normal",
+};
