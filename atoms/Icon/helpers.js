56aed0e152d77bcef91b5efb64b5808a43d81dd4
alan <alanarostegui@hotmail.com>
2021-11-01 19:13:52 -0600

    style: add icon styles

--- atoms/Icon/helpers.js
+++ atoms/Icon/helpers.js
@@ -4,11 +4,5 @@ const iconSize = {
   lg: 50,
   xl: 70,
 };
-const iconColor = {
-  base: "#ff0000",
-  highlight: "",
-  muted: "",
-  primary: "",
-};
-export const mapColor = (color) => iconColor[color];
+
 export const mapSize = (size) => iconSize[size];
